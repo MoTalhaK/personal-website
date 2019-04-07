f0ef0f64dd06af2f40033f4b5e707e9057884abf
Talha <muhammad.t.k@hotmail.co.uk>
2019-04-07 01:32:54 -0400

    styling

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,10 +1,20 @@
 import React, {Component} from "react";
+// import Background from './images/gray_sand.png';
+
+let backgroundUrl = require('./images/ag-square.png');
+
+const divStyle = {
+    backgroundImage: 'url(' + backgroundUrl + ')',
+    backgroundColor: "#00a6ff",
+    width: '100%',
+    height: '850px'
+};
 
 class Home extends Component {
     render() {
         return (
-            <div>
-                Home Page
+            <div style={divStyle}>
+
             </div>
         )
     }
