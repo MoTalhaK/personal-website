bb2d423c1c001d83b532928ca114ddd83f556fa4
Talha <muhammad.t.k@hotmail.co.uk>
2019-04-20 01:40:05 -0400

    changed css for various components

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import {BrowserRouter as Router, Link} from 'react-router-dom';
 import {Route} from 'react-router-dom';
 import Home from './components/home-component/home';
 import NavBar from './components/header-component/NavBar';
+import About from './components/about-component/about';
 import Footer from './components/footer-component/footer';
-import {Nav} from 'react-bootstrap';
-// import './styles.css';
+import './App.css';
 
 class App extends Component {
     render() {
         return (
             <Router>
-                <div
-                    style={{
-                        position: "absolute",
-                        top: 0,
-                        left: 0,
-                        width: "100%",
-                        height: "100%"
-                    }}
-                >
-                    <NavBar/>
-                    <Route name="home" exact path="/" component={Home}/>
-                    <Footer/>
+                <div className="wrapper">
+                    <div className="section home-section">
+                        <NavBar/>
+                        <div className="section-container home-component">
+                            <Route name="home" exact path="/" component={Home}/>
+                        </div>
+                    </div>
+                    <div className="section about-section">
+                        <div className="section-container about-component">
+                            <About/>
+                        </div>
+                    </div>
                 </div>
             </Router>
         );
