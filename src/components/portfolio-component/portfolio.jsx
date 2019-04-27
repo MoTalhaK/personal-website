acb42c136d758107df71d63601a707cc57fcec4b
Talha <muhammad.t.k@hotmail.co.uk>
2019-04-26 23:36:10 -0400

    portfolio section

---
+++ src/components/portfolio-component/portfolio.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './portfolio.css';
+
+class Portfolio extends Component {
+    render() {
+        return (
+            <div className="portfolio-container">
+                <h2 id="header-title">PORTFOLIO</h2>
+            </div>
+        )
+    }
+}
+
+export default Portfolio
