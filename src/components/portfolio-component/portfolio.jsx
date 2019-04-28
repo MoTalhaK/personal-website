0e5c9dded83efe129f478ac2816b71d79c5abca6
Talha <muhammad.t.k@hotmail.co.uk>
2019-04-28 17:55:01 -0400

    placeholder images in portfolio section

--- src/components/portfolio-component/portfolio.jsx
+++ src/components/portfolio-component/portfolio.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {Image} from 'react-bootstrap';
+import {Container, Row, Col} from 'react-bootstrap';
 import './portfolio.css';
 
 class Portfolio extends Component {
@@ -6,6 +8,35 @@ class Portfolio extends Component {
         return (
             <div className="portfolio-container">
                 <h2 id="header-title">PORTFOLIO</h2>
+                <p className="portfolio-text">Here are a few things I've created recently.</p>
+                <Container>
+                    <Row>
+                        <Col xs={6} md={4}>
+                            <Image
+                                src="https://cdn.pixabay.com/photo/2015/08/18/09/54/canada-893534_640.jpg"
+                                rounded
+                                width={350}
+                                height={350}
+                            />
+                        </Col>
+                        <Col xs={6} md={4}>
+                            <Image
+                                src="https://cdn.pixabay.com/photo/2015/08/18/09/54/canada-893534_640.jpg"
+                                rounded
+                                width={350}
+                                height={350}
+                            />
+                        </Col>
+                        <Col xs={6} md={4}>
+                            <Image
+                                src="https://cdn.pixabay.com/photo/2015/08/18/09/54/canada-893534_640.jpg"
+                                rounded
+                                width={350}
+                                height={350}
+                            />
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         )
     }
