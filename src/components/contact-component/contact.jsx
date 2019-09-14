97b026eaee843c44c69c6517122ce86379ac9e4f
Muhammad <muhammad.t.k@hotmail.co.uk>
2019-09-13 21:24:59 -0400

    form validation

--- src/components/contact-component/contact.jsx
+++ src/components/contact-component/contact.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Form from 'react-bootstrap/Form';
 import {Button} from 'react-bootstrap';
+import {useState} from 'react';
 import axios from "axios";
 import './contact.css';
 
@@ -25,6 +26,7 @@ class Contact extends Component {
                     // handle success
                 } else {
                     // handle error
+
                     console.log(response.data.message);
                 }
 
@@ -36,6 +38,19 @@ class Contact extends Component {
         e.preventDefault();
     };
 
+    formValidation() {
+        const [validated, setValidated] = useState(false);
+
+        const handleSubmit = event => {
+            const form = event.currentTarget;
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            setValidated(true);
+        }
+    };
+
     handleFields = e => this.setState({[e.target.name]: e.target.value});
 
     render() {
@@ -44,16 +59,33 @@ class Contact extends Component {
                 <h2 id="header-title">CONTACT</h2>
                 <p className="contact-text">Let's get in touch!</p>
                 <div className="form-container">
-                    <Form onSubmit={this.handleForm}>
+                    <Form onSubmit={function (event) {
+                        // event.handleForm();
+                        // event.formValidation();
+                    }}>
                         <Form.Group controlId="formGroupName">
-                            <Form.Control name="fname" type="name" placeholder="Name" onChange={this.handleFields}/>
+                            <Form.Control name="fname"
+                                          type="name"
+                                          placeholder="Name"
+                                          onChange={this.handleFields}
+                                          required/>
+                            <Form.Control.Feedback>Nice</Form.Control.Feedback>
                         </Form.Group>
                         <Form.Group controlId="formGroupEmail">
-                            <Form.Control name="femail" type="email" placeholder="Email" onChange={this.handleFields}/>
+                            <Form.Control name="femail"
+                                          type="email"
+                                          placeholder="Email"
+                                          onChange={this.handleFields}
+                                          required/>
                         </Form.Group>
                         <Form.Group controlId="formGroupMessage">
-                            <Form.Control name="fmessage" as="textarea" rows="6" type="message" placeholder="Message"
-                                          onChange={this.handleFields}/>
+                            <Form.Control name="fmessage"
+                                          as="textarea"
+                                          rows="6"
+                                          type="message"
+                                          placeholder="Message"
+                                          onChange={this.handleFields}
+                                          required/>
                         </Form.Group>
                         <div className="submit-div">
                             <Button variant="secondary" type="submit" className="btn-submit">
