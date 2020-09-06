b903d69a972d9bb5d4085fa9185ffbf9be853427
Talha <muhammad.t.k@hotmail.co.uk>
2020-09-05 23:40:38 -0400

    changes for heroku

--- api/index.js
+++ api/index.js
@@ -1,9 +1,17 @@
 var express = require("express");
 var router = express.Router();
 var nodemailer = require("nodemailer");
-var cors = require("cors");
 var path = require("path");
 const creds = require("./config");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const buildPath = path.join(__dirname, '..', 'build');
+
+app.use(express.static(buildPath));
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 var transport = {
     host: "smtp.gmail.com",
@@ -50,15 +58,15 @@ router.post("/send", (req, res, next) => {
     });
 });
 
-const app = express();
 
-app.use(express.static(path.join(__dirname, '/build')));
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/build/index.html'));
-});
+// app.use(express.static(path.join(__dirname, '/build')));
+// app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname+'/build/index.html'));
+// });
 
-app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(3002);
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`);
+});
 
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Link} from 'react-router-dom';
+import {HashRouter as Router, Link} from 'react-router-dom';
 import {Route} from 'react-router-dom';
 import Home from './components/home-component/home';
 import About from './components/about-component/about';
@@ -12,40 +12,39 @@ import Skills from "./components/skills-component/skills";
 class App extends Component {
     render() {
         return (
-            <Router>
-                <div className="wrapper">
-                    <div className="section home-section">
-                        <div className="section-container home-component">
-                            <Route name="home" exact path="/" component={Home}/>
-                        </div>
-                    </div>
-                    <div className="section about-section">
-                        <div className="section-container about-component">
-                            <About/>
-                        </div>
-                    </div>
-                    <div className="section skills-section">
-                        <div className="section-container skills-component">
-                            <Skills/>
-                        </div>
-                    </div>
-                    <div className="section portfolio-section">
-                        <div className="section-container portfolio-component">
-                            <Portfolio/>
-                        </div>
-                    </div>
-                    <div className="section contact-section">
-                        <div className="section-container contact-component">
-                            <Contact/>
-                        </div>
-                    </div>
-                    <div className="section footer-section">
-                        <div className="section-container footer-component">
-                            <Footer/>
-                        </div>
+            <div className="wrapper">
+                <div className="section home-section">
+                    <div className="section-container home-component">
+                        {/*<Route name="home" exact path="/" component={Home}/>*/}
+                        <Home/>
                     </div>
                 </div>
-            </Router>
+                <div className="section about-section">
+                    <div className="section-container about-component">
+                        <About/>
+                    </div>
+                </div>
+                <div className="section skills-section">
+                    <div className="section-container skills-component">
+                        <Skills/>
+                    </div>
+                </div>
+                <div className="section portfolio-section">
+                    <div className="section-container portfolio-component">
+                        <Portfolio/>
+                    </div>
+                </div>
+                <div className="section contact-section">
+                    <div className="section-container contact-component">
+                        <Contact/>
+                    </div>
+                </div>
+                <div className="section footer-section">
+                    <div className="section-container footer-component">
+                        <Footer/>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
