48c14c784c515df6d9a9cfcb43d7e742f383457e
Talha <muhammad.t.k@hotmail.co.uk>
2020-09-08 01:41:43 -0400

    changes for heroku

---
+++ client/build/precache-manifest.66f3a61dcf4a35c44cb14c70900035db.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "00212d9a8a368f458d0fdf5077ad9e6d",
+    "url": "./static/media/starrysky.00212d9a.png"
+  },
+  {
+    "revision": "b06c42492d0422c0fbb1",
+    "url": "./static/css/main.3df845df.chunk.css"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "c9d51a10dfb76c71c41e409af3c21878",
+    "url": "./static/media/random_forests.c9d51a10.PNG"
+  },
+  {
+    "revision": "2e27f7ed35e630c79cb7",
+    "url": "./static/js/2.00f5b446.chunk.js"
+  },
+  {
+    "revision": "26dbbe0f67695b782745fe672128cc40",
+    "url": "./static/media/oefen.26dbbe0f.png"
+  },
+  {
+    "revision": "3383d6795384690ac52ada1c08508445",
+    "url": "./static/media/spellsword.3383d679.PNG"
+  },
+  {
+    "revision": "b06c42492d0422c0fbb1",
+    "url": "./static/js/main.32210f79.chunk.js"
+  },
+  {
+    "revision": "25cc465220f2fffe10bc919f8003554a",
+    "url": "./static/media/personal_website.25cc4652.png"
+  },
+  {
+    "revision": "12191170a587275a4e940a0e33e1767f",
+    "url": "./static/media/10_means_max.12191170.png"
+  },
+  {
+    "revision": "c5800b118fd507090cdcd6c22c3eb4ab",
+    "url": "./static/media/defect_prediction.c5800b11.PNG"
+  },
+  {
+    "revision": "82cbe14eeb9cf6ac15c9e5b2a1d5baa5",
+    "url": "./static/media/5_means_max.82cbe14e.png"
+  },
+  {
+    "revision": "1482611bdacad555741aeebfe4992947",
+    "url": "./static/media/5_means_max_2D.1482611b.png"
+  },
+  {
+    "revision": "2e27f7ed35e630c79cb7",
+    "url": "./static/css/2.32756089.chunk.css"
+  },
+  {
+    "revision": "7aa3a19adb9020c1173afaaf60cbb9df",
+    "url": "./index.html"
+  }
+];
